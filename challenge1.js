bf7404becd07ac0428de82208cbaacc2f8dd5538
phoebe velma awuor <awuorphoebi@gmail.com>
2024-11-30 10:51:04 +0300

    challenge1.js

---
+++ challenge1.js
@@ -0,0 +1,25 @@
+//intoduce the function
+function outputGrade(score){
+if (score < 0 || score > 100) {
+   //alert message incase they input a number <0 or >100
+    alert("Invalid input! Please check your entry.");
+    return;
+}
+//determine the grade
+let  grade;
+if (score>79) {
+    grade="A";
+}else if (score >= 60) {
+    grade="B";
+} else if (score >= 49) {
+    grade="C";
+}else if (score >= 40) {
+    grade="D";
+}else {
+    grade="E";
+}
+return grade;
+}
+//call function
+console.log(outputGrade(59))
+  
