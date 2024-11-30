23375a3da423af2ce61a23af4d5f55772c883892
phoebe velma awuor <awuorphoebi@gmail.com>
2024-11-30 13:21:55 +0300

    challenge2.js

---
+++ challenge2.js
@@ -0,0 +1,18 @@
+function checkSpeed(speed){
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+    if (speed <= speedLimit) {
+        return "Ok";
+    }
+    //loop the function 
+    let points = 0;
+    for (let currentSpeed = speedLimit + 1; currentSpeed <= speed; currentSpeed += kmPerPoint) 
+        points++;
+
+    if (points > 12) {
+        return "License suspended";
+    } else {
+        return `Points: ${points}`;
+    }
+}
+console.log(checkSpeed(135));
