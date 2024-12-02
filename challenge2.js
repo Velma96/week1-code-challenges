66dd35d982e4d96779a94a1546f39e8e5c1cbf65
phoebe velma awuor <awuorphoebi@gmail.com>
2024-12-02 09:08:44 +0300

    few changes

--- challenge2.js
+++ challenge2.js
@@ -8,11 +8,12 @@ function checkSpeed(speed){
     let points = 0;
     for (let currentSpeed = speedLimit + 1; currentSpeed <= speed; currentSpeed += kmPerPoint) 
         points++;
-
+    //check if points exceed the suspensuion threshold
     if (points > 12) {
         return "License suspended";
     } else {
         return `Points: ${points}`;
     }
 }
+//test the function with a sample speed 
 console.log(checkSpeed(85));
